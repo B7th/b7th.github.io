3808a17a3c1f6d3d0c025341b0af839c8202d652
Martin Gagnon <b77mg@icloud.com>
2019-11-26 00:43:54 -0800

    Standardized

--- mlharp/12tone.js
+++ mlharp/12tone.js
@@ -1,5 +1,5 @@
 (function() {
-  var notes;
+  var i, ids, keys, len, notes, values;
 
   notes = {
     '#C1': 'C3',
@@ -40,20 +40,78 @@
     '#Eb3': 'Eb5'
   };
 
+  ids = {};
+
+  for (values = i = 0, len = notes.length; i < len; values = ++i) {
+    keys = notes[values];
+    ids[values] = keys;
+  }
+
+  keys = {
+    49: 'C5',
+    50: 'G5',
+    51: 'D5',
+    52: 'A5',
+    53: 'E5',
+    54: 'B5',
+    55: 'Gb5',
+    56: 'Db5',
+    57: 'Ab5',
+    48: 'Eb5',
+    173: 'Bb5',
+    61: 'F5',
+    81: 'C4',
+    87: 'G4',
+    69: 'D4',
+    82: 'A4',
+    84: 'E4',
+    89: 'B4',
+    85: 'Gb4',
+    73: 'Db4',
+    79: 'Ab4',
+    80: 'Eb4',
+    219: 'Bb4',
+    221: 'F4',
+    65: 'C3',
+    83: 'G3',
+    68: 'D3',
+    70: 'A3',
+    71: 'E3',
+    72: 'B3',
+    74: 'Gb3',
+    75: 'Db3',
+    76: 'Ab3',
+    59: 'Eb3',
+    222: 'Bb3',
+    13: 'F3'
+  };
+
   $(window).on("load", function() {
-    var noteEnd, noteId, noteName, noteStart, synth, synthJSON;
+    var noteEnd, noteId, noteName, noteStart, notesPlaying, synth, synthJSON;
     synth = {};
     synthJSON = {
       envelope: {
-        attack: 0.1,
-        decay: 0.2,
+        attack: 0.05,
+        attackCurve: 'exponential',
+        decay: .4,
+        decayCurve: 'exponential',
         sustain: 1,
         release: 0.8
+      },
+      oscillator: {
+        type: 'custom',
+        phase: 1,
+        partials: [1]
       }
     };
+//reverb:
+//  decay:
     for (noteId in notes) {
       noteName = notes[noteId];
       $(noteId).data('note', noteName);
+      $(noteId).css({
+        cursor: 'pointer'
+      });
       $(noteId).mousedown(function() {
         return noteStart($(this).data('note'));
       });
@@ -66,16 +124,36 @@
       $(noteId).on('touchend', function() {
         return noteEnd($(this).data('note'));
       });
-      $(noteId).css({
-        cursor: 'pointer'
-      });
-      synth[noteName] = new Tone.Synth(synthJSON).toMaster();
     }
+    $('body').on('keydown', function(e) {
+      if (keys[e.which] != null) {
+        noteStart(keys[e.which]);
+      }
+      return false;
+    });
+    $('body').on('keyup', function(e) {
+      if (keys[e.which] != null) {
+        noteEnd(keys[e.which]);
+      }
+      return false;
+    });
+    synth = new Tone.PolySynth(synthJSON).toMaster();
+    notesPlaying = {};
     noteStart = function(noteName) {
-      return synth[noteName].triggerAttack(noteName);
+      if (notesPlaying[noteName]) {
+        return;
+      }
+      notesPlaying[noteName] = true;
+      return synth.triggerAttack(noteName);
     };
+    //$(ids[noteName]).style.fill = "#fff"
+    //$(ids[noteName]).style.stroke= "#fff"
     return noteEnd = function(noteName) {
-      return synth[noteName].triggerRelease();
+      notesPlaying[noteName] = false;
+      synth.triggerRelease(noteName);
+      return $(ids[noteName]).css({
+        "fill": ""
+      });
     };
   });
 
